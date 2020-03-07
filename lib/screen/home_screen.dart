a7c075c1224d5a7f8f06225720dbb0db3b87a372
ankittale <ankittlabs1109@gmail.com>
2020-03-07 16:08:53 +0530

    Completed the Budget App UI

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:pocket_buggy/data/data.dart';
+import 'package:pocket_buggy/helpers/color_helper.dart';
+import 'package:pocket_buggy/model/category_model.dart';
+import 'package:pocket_buggy/model/expense_model.dart';
 import 'package:pocket_buggy/widgets/bar_chart.dart';
+
+import 'category_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
@@ -11,6 +16,79 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<double> weeklyExpending;
+
+  _buildCategory(Category category, double totalAmountExpense) {
+    return GestureDetector(
+      onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (_) => CategoryScreen(category: category))),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        padding: EdgeInsets.all(20.0),
+        height: 100.0,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black12, offset: Offset(0, 2), blurRadius: 6.0)
+            ]),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  category.name,
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+                ),
+                Text(
+                  '\$${(category.maxAmount - totalAmountExpense).toStringAsFixed(2)} / \$${(category.maxAmount).toStringAsFixed(2)}',
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraint) {
+                final double maxWidth = constraint.maxWidth;
+                final double percent =
+                    (category.maxAmount - totalAmountExpense) /
+                        category.maxAmount;
+                double barWidth = percent * maxWidth;
+
+                if (barWidth < 0) {
+                  barWidth = 0;
+                }
+                return Stack(
+                  children: <Widget>[
+                    Container(
+                      height: 20.0,
+                      decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(15.0)),
+                    ),
+                    Container(
+                      height: 20.0,
+                      width: barWidth,
+                      decoration: BoxDecoration(
+                          color: getColor(context, percent),
+                          borderRadius: BorderRadius.circular(15.0)),
+                    )
+                  ],
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,23 +115,31 @@ class _HomeScreenState extends State<HomeScreen> {
             SliverList(
                 delegate: SliverChildBuilderDelegate(
               (BuildContext context, int index) {
-                return Container(
-                  margin: EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black12,
-                        offset: Offset(0, 2),
-                        blurRadius: 6.0
-                      )
-                    ]
-                  ),
-                  child: BarChart(weeklySpending),
-                );
+                if (index == 0) {
+                  return Container(
+                    margin: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12,
+                              offset: Offset(0, 2),
+                              blurRadius: 6.0)
+                        ]),
+                    child: BarChart(weeklySpending),
+                  );
+                } else {
+                  final Category category = categories[index - 1];
+                  double totalAmountSpent = 0;
+                  category.expenses.forEach((Expense expense) {
+                    totalAmountSpent += expense.cost;
+                  });
+
+                  return _buildCategory(category, totalAmountSpent);
+                }
               },
-              childCount: 1,
+              childCount: 1 * categories.length,
             ))
           ],
         ),
